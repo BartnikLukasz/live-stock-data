fa229250c75c5e0830aa3e8f31a5280a8a332de6
BartnikLukasz <bartnik.luk@gmail.com>
2026-04-08 09:47:35 +0200

    Project setup

---
+++ alerts/src/main/java/org/bartnik/alerts/AlertsApplication.java
@@ -0,0 +1,13 @@
+package org.bartnik.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertsApplication.class, args);
+    }
+
+}
